319178dbba15fc2329336d5de5c773eb1118f1f4
Marcela Cunha <marcelacunha@Gabriels-MacBook-Pro.local>
2023-10-16 15:01:10 -0300

    Commit inicial

---
+++ EllaApp/Presentacao/Inicio/ViewControllerInicial.swift
@@ -0,0 +1,16 @@
+//
+//  ViewControllerInicial.swift
+//  EllaApp
+//
+//  Created by Marcela Cunha on 16/10/23.
+//
+
+import UIKit
+
+class ViewControllerInicial: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '16.0'
+
+target 'EllaApp' do
+  use_frameworks!
+  pod 'Kingfisher'
+end
