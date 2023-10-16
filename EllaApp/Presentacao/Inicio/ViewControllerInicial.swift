2b16b9c5312737f6bd1d30d5fff3f8471cf5c927
Marcela Cunha <marcelacunhaletras013@gmail.com>
2023-10-16 16:03:27 -0300

    dia1

--- EllaApp/Presentacao/Inicio/ViewControllerInicial.swift
+++ EllaApp/Presentacao/Inicio/ViewControllerInicial.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class ViewControllerInicial: UIViewController {
 
+    @IBOutlet weak var bemvindeLabel: UILabel!
+    @IBOutlet weak var perfilImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        bemvindeLabel.text = "Bem-vinde, Marcela :)"
+        perfilImageView.layer.cornerRadius = 64
+    }
+    
+    
+    @IBAction func aoApertarIniciar(_ sender: Any) {
+        view.backgroundColor = .white
+        bemvindeLabel.textColor = .black
     }
 }
